daa8cfed7eb0cef28c15ec504581e497db54e5ed
Aleksandr <30931276+rybakow@users.noreply.github.com>
2023-08-15 09:35:39 +0300

    bugfix
    "1. EnemySpawnController - Не используйте обобщения по типу Manager, Controller и т.д. Такое именование классам дают как правило когда класс хранит в себе лишние ответственности, отсюда и стараются давать обобщённые именования"

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,8 +1,7 @@
-using System;
 using System.Collections;
 using UnityEngine;
 
-public class EnemySpawnController : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private float _timeToSpawn;
     [SerializeField] private Enemy _enemyPrefab;
